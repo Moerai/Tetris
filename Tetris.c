9e0173391ce2de1cc0c5b9d705e2f6df7d1d0c49
KrToTo <friendnt@naver.com>
2018-06-13 01:00:43 +0900

    렌덤 블럭생성, 블럭 내려옴

--- Tetris.c
+++ Tetris.c
@@ -11,21 +11,23 @@ void InitNewBlock(char blockInfo[][4]);
 void ShowBlock(char blockInfo[][4]);
 /*블럭을 삭제*/
 void DeleteBlock(char blockInfo[][4]);
-/*블럭을 내림*/
-void DownBlock(char blockInfo[][4]);
 /*블럭을 바꿈*/
 /*블럭을 멈춤*/
 /*가득찬 줄 삭제*/
 
 /*맵그리기*/
-void DrowMap(char map[23][27]);
+void DrowMap(char map[15][27]);
 
-int point_x,point_y;
+int point_x, point_y; //벽돌을 그려주기 위한 포인트
+int cur_x =0;//커서의 위치
+int *cur_xPtr=&cur_x;
+int cur_y =6;
+int *cur_yPtr=&cur_y;
 int x,y;
-char map[23][27];
+char map[15][27];
 char blockModel[][4][4] =
 {//Idia 4차원 배열을 이용하면 호출할 때 좀 더 용이하지 않을까??
-	/*첫 번째 블럭0,1,2,3
+	/*첫 번째 블럭1,2,3,4
 	  ■
   	  ■ ■ ■   */
 	{
@@ -52,7 +54,7 @@ char blockModel[][4][4] =
 	  {0,0,1,0},
 	  {0,1,1,0}
 	},
-	/*두 번째 블럭4,5,6,7
+	/*두 번째 블럭5,6,7,8
 	      ■
 	  ■ ■ ■    */
 	{
@@ -79,7 +81,7 @@ char blockModel[][4][4] =
 	  {0,1,1,0},
 	  {0,0,0,0}
 	},
-	/*세 번째 블럭8,9,10,11
+	/*세 번째 블럭9,10,11,12
 		■
 		■
 		■
@@ -108,7 +110,7 @@ char blockModel[][4][4] =
 	  {1,1,1,1},
 	  {0,0,0,0}
 	},
-	/*네 번째 블럭12,13
+	/*네 번째 블럭13,14
 	  ■ ■
 	    ■ ■  */
 	{
@@ -123,7 +125,7 @@ char blockModel[][4][4] =
 	  {0,1,0,0},
 	  {0,0,0,0}
 	},
-	/*다섯 번째 블럭14,15
+	/*다섯 번째 블럭15,16
 	   ■ ■
 	 ■ ■  */
 	{
@@ -138,7 +140,7 @@ char blockModel[][4][4] =
 	  {0,0,1,0},
 	  {0,0,0,0}
 	},
-	/*여섯 번째 블럭16,17,18,19
+	/*여섯 번째 블럭17,18,19,20
 	   ■
 	 ■ ■ ■  */
 	{
@@ -165,7 +167,7 @@ char blockModel[][4][4] =
 	  {0,1,1,0},
 	  {0,0,1,0}
 	},
-	/*일곱 번째 블럭 20
+	/*일곱 번째 블럭 21
 	  ■ ■
 	  ■ ■
 	*/
@@ -179,28 +181,20 @@ char blockModel[][4][4] =
 
 int main(void)
 {
-	setlocale(LC_CTYPE, "ko.KR.utf-8");
+	int i,j;//블럭이 내려오게 하기 위해 씀, 블럭 모델을 선택하기 위해 씀
+	setlocale(LC_CTYPE, "ko.KR.utf-8");//한글 사용을 위해 사용
+	j = rand()%21;
 	initscr();
-	move(0,0);
-	addch(ACS_CKBOARD);
+	//맵을 그리고
+	while(true){
+	i++;
+	InitNewBlock(blockModel[j]);
 	refresh();
 	sleep(1);
-	point_x = 3; point_y = 4;
-	ShowBlock(blockModel[0]);
+	DeleteBlock(blockModel[j]);
 	refresh();
-	sleep(1);
-	point_x = 3; point_y = 8;
-	ShowBlock(blockModel[1]);
-	refresh();
-	sleep(1);
-	point_x = 3; point_y = 12;
-	ShowBlock(blockModel[2]);
-	refresh();
-	sleep(1);
-	point_x = 3; point_y = 16;
-	ShowBlock(blockModel[3]);
-	refresh();
-	sleep(1);
+	cur_x=+i;
+	}
 	endwin();
 	return 0;
 }
@@ -213,8 +207,38 @@ void ShowBlock(char blockInfo[][4])
 			if(blockInfo[y][x]==1)
 			{
 				curs_set(0);
-				move(point_x+y,point_y+x);
+				move(point_x+x,point_y+y);
 				addch(ACS_CKBOARD);
+			}
+		}
+	}
+}
+void InitNewBlock(char blockInfo[][4])
+{
+	for(y=0; y<4; y++)
+	{
+		for(x=0; x<4; x++)
+		{
+			if(blockInfo[y][x]==1)
+			{
+				curs_set(0);
+				move(cur_x+x,cur_y+y);
+				addch(ACS_CKBOARD);	
+			}
+		}
+	}
+}
+void DeleteBlock(char blockInfo[][4])
+{
+	for(y=0; y<4; y++)
+	{
+		for(x=0; x<4; x++)
+		{
+			if(blockInfo[y][x]==1)
+			{
+				curs_set(0);
+				move(cur_x+x,cur_y+y);
+				printw("%s"," ");
 			}
 		}
 	}
