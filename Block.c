379173c762add8a0fb8a8c8af251791b1317498c
KrToTo <friendnt@naver.com>
2018-06-12 15:41:26 +0900

    특수문자 이용하여 블록 만들기 성공

--- Block.c
+++ Block.c
@@ -6,7 +6,8 @@
 
 void ShowBlock(char blockInfo[][4]);
 
-
+int point_x,point_y;
+int x,y;
 char blockModel[][4][4] =
 {
 	/*a
@@ -42,11 +43,23 @@ int main(void)
 	setlocale(LC_CTYPE, "ko.KR.utf-8");
 	initscr();
 	move(0,0);
-	printw("%s","■");
+	addch(ACS_CKBOARD);
 	refresh();
 	sleep(1);
-	move(3,4);
+	point_x = 3; point_y = 4;
 	ShowBlock(blockModel[0]);
+	refresh();
+	sleep(1);
+	point_x = 3; point_y = 8;
+	ShowBlock(blockModel[1]);
+	refresh();
+	sleep(1);
+	point_x = 3; point_y = 12;
+	ShowBlock(blockModel[2]);
+	refresh();
+	sleep(1);
+	point_x = 3; point_y = 16;
+	ShowBlock(blockModel[3]);
 	refresh();
 	sleep(1);
 	endwin();
@@ -54,15 +67,15 @@ int main(void)
 }
 void ShowBlock(char blockInfo[][4])
 {
-	int x, y;
-	
 	for(y=0; y<4; y++)
 	{
 		for(x=0; x<4; x++)
 		{
 			if(blockInfo[y][x]==1)
 			{
-				printw("%s","■");
+				curs_set(0);
+				move(point_x+y,point_y+x);
+				addch(ACS_CKBOARD);
 			}
 		}
 	}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is Tetris Game for Linux and it is helpful to attend a lecture named analysis of a program of DongYang Mire University.
+
+You can compile this gcc -o <name you want> <Allfile> -lncursesw
